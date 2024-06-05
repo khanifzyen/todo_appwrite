9f4e78e097b7b153bc944d3150b6acca92c20ae7
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2024-06-05 14:56:23 +0700

    initial commit

---
+++ lib/constants.example.dart
@@ -0,0 +1,4 @@
+// lib/constants.dart
+const String endpoint = 'https://<YOUR_APPWRITE_ENDPOINT>';
+const String projectId = '<YOUR_PROJECT_ID>';
+const String collectionId = '<YOUR_COLLECTION_ID>';
